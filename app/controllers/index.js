3a1070f90edfe1e4e604bc7339879967ab87cb7a
Thomas Bormans <thomas.bormans@district01.be>
2017-05-18 09:04:31 +0200

    Require update controller by default

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,11 @@
 var helpers = require("./helpers");
 var create = require("./create");
+var update = require("./update");
 var remove = require("./remove");
 
 module.exports = {
     helpers: helpers,
     create: create,
+    update: update,
     remove: remove
 };
