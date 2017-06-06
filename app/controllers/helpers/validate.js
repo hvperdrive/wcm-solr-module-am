6c22ba51f2249e48911b50202401fcb178b21175
Thomas Bormans <thomas.bormans@district01.be>
2017-06-06 15:42:20 +0200

    Fix validating content type

--- app/controllers/helpers/validate.js
+++ app/controllers/helpers/validate.js
@@ -2,5 +2,5 @@ var _ = require("lodash");
 
 module.exports = function(data) {
     // Check if we need to call the solr API based on safeLabel of CT
-    return (_.get(data, "meta.contentType.meta.safeLabel") && data.meta.contentType.meta.safeLabel === "projects");
+    return (_.get(data, "meta.contentType.meta.safeLabel") && data.meta.contentType.meta.safeLabel.toLowerCase() === "projecten");
 };
